d949a592e954fb41d64e582f77fb6dff65991dc6
TwoOnefour <lys214412@gmail.com>
2023-04-10 02:46:21 +0800

    update

--- __init__.py
+++ __init__.py
@@ -9,21 +9,12 @@ class Logistic:
         self.csv_data = None   # 数据
         self.test_data = None  # 测试集
         self.train_data = None  # 训练集
-        self.times = 50   # 迭代次数
+        self.times = 1000   # 迭代次数
         self.a = 1 / self.times ** 0.8  # 步长阿尔法
         self.w = None # 结果矩阵
-        self.target = None # 梯度下降
-        self.o1 = None
-        self.o2 = 0.5   # 初始化为0.5
-        self.b = []
+
     def sigmoid(self, z):
         return 1 / (1 + math.e ** ((-1) * z))
-
-    def linear(self, x1, x2, o1, o2):
-        return o1 * x1 + o2 * x2
-
-    def cost(self):
-        pass
 
     def read_data(self):
         self.csv_data = pd.read_csv("./作业数据集.csv")
@@ -31,48 +22,46 @@ class Logistic:
         self.csv_data = self.csv_data[self.csv_data.columns[:-1]]
         self.train_data = int(len(self.csv_data) / 3 * 2)
         self.train_data = self.csv_data[:int(len(self.csv_data) / 3 * 2)]
-        self.test_data = len(self.csv_data) - self.train_data
-        self.test_data = self.csv_data[:len(self.csv_data) - len(self.train_data)]
+        # self.test_data = len(self.csv_data) - len(self.train_data)
+        self.test_data = self.csv_data[len(self.train_data):]
         # self.w = np.transpose(np.array([[1] * 12]))
 
     def classify(self):
         m, n = np.shape(self.train_data)
-        self.w = np.ones((n, 1))
-
+        self.w = np.ones((1, n))
         # self.sigmoid(self.w * )
         for i in range(self.times):
-            for j in range(m):
             # self.w = np.linalg.solve(self.csv_data.get, b)
-                hx = self.sigmoid(np.sum(np.multiply(self.w, self.train_data[j:j+1].T))[0])
-                diff = self.result[j] - hx
-                self.w = self.w + self.a * diff * self.train_data[j:j+1].T
+            hx = self.sigmoid(np.sum(np.multiply(np.array(self.train_data), self.w), axis=1))
+            diff = -np.array(self.result[:int(len(self.csv_data) / 3 * 2)]) + hx
+            self.w = self.w - np.array([np.dot(diff, np.array(self.train_data))]) * self.a
                 # print(1)
-
-    def stocGradAscent1(self, dataMatrix, classLabels, numIter=150):
-        m, n = np.shape(dataMatrix)
-        weights = np.ones(n)  # initialize to all ones
-        for j in range(numIter):
-            dataIndex = list(range(m))
-            for i in range(m):
-                alpha = 4 / (1.0 + j + i) + 0.0001  # apha decreases with iteration, does not
-                randIndex = int(np.random.uniform(0, len(dataIndex)))  # go to 0 because of the constant
-                h = self.sigmoid(sum(dataMatrix[randIndex:randIndex+1] * weights))
-                error = classLabels[randIndex] - h
-                weights = weights + alpha * error * dataMatrix[randIndex]
-                del (dataIndex[randIndex])
-        return weights
-
 
     def predict(self):
         m, n = np.shape(self.test_data)
-        correct = 0
+        TP = 0
+        FP = 0
+        FN = 0
+        TN = 0
+        accuracy = 0
         for i in range(m):
-            hx = self.sigmoid(np.sum(np.multiply(self.w, self.test_data[i:i+1].T)))
-            if abs(hx - self.result[i]) < 0.00001:
-                correct += 1
-        print("正确率为：{}".format(correct / m))
+            hx = self.sigmoid(np.sum(np.multiply(self.w, np.array(self.test_data[i:i+1]))))
+            if self.result[i + len(self.train_data)] == 1:
+                if abs(hx - self.result[i + len(self.train_data)]) < 0.0000001:
+                    TP += 1
+                else:
+                    FP += 1
+            else:
+                if abs(hx - self.result[i + len(self.train_data)]) < 0.0000001:
+                    TN += 1
+                else:
+                    FN += 1
+        print("准确率为：{:.2f}".format((TP+TN) / m))
+        print("精确率为：{:.2f}".format(TP / (TP + FP)))
+        print("召回率为：{:.2f}".format(TP / (TP + FN)))
+        print("F1-score为：{:.2f}".format(2 * (TP / (TP + FP)) * (TP / (TP + FN)) / ((TP / (TP + FN)) + (TP / (TP + FP)))))
 
     def run(self):
         self.read_data()
-        self.stocGradAscent1(self.train_data, self.result)
+        self.classify()
         self.predict()
